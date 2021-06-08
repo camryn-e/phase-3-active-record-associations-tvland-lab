757109db2061aac31dda4703f857db896aa8c212
camryn williams <williams.camryn.e@gmail.com>
2021-06-08 16:05:37 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,18 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    arr = []
+    self.characters.all.each { |a| arr << "#{a.name} - #{a.show.name}"}
+    arr
+  end
+
+
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,19 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+
+  def actors_list
+    arr = []
+    self.actors.all.each { |a| arr << "#{a.full_name}"}
+    arr
+  end
+
+  def build_network(new_network)
+    n = Network.find_or_create_by(new_network)
+    self.network = n
+    self.network
+  end
+
 end
